67ad7f481f8509695743b90f450f575f3ffdd133
Jenny Kim <kimj1905@gmail.com>
2021-11-02 16:56:10 -0400

    commit tree ff7a9d4996915a2a74cc6f5262f77b937f551be7

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,30 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({ message: e.target.value });
+    // console.log(this.state.message.length);
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          id="message"
+          // onChange={(e) => this.handleChange(e)}
+          onChange={this.handleChange}
+          value={this.state.message}
+        />
+        <div>
+          Character count: {this.props.maxChars - this.state.message.length}
+        </div>
       </div>
     );
   }
